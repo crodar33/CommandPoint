b6ca697f7fa484276ef2dcc68a914237bc5e8a59
Dandi <mcrodar@gmail.com>
2021-09-26 14:24:58 +0300

    restart if no wifi

--- init.lua
+++ init.lua
@@ -13,7 +13,7 @@ function dataToString(data)
     end
     return result
 end
-
+ 
 --[[
 ntpTime = 0
 local wifiGotIpEvent = function(T)
@@ -35,40 +35,42 @@ local wifiGotIpEvent = function(T)
 end
 ]]
 local wifiConnectEvent = function(T)
-  print("Connection to AP("..T.SSID..") established!")
-  print("Waiting for IP address...")
-  if disconnect_ct ~= nil then disconnect_ct = nil end
+    print("Connection to AP("..T.SSID..") established!")
+    print("Waiting for IP address...")
+    if disconnect_ct ~= nil then disconnect_ct = nil end
 end
 wifiDisconnectEvent = function(T)
-  if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
-    --the station has disassociated from a previously connected AP
-    return
-  end
-  -- total_tries: how many times the station will attempt to connect to the AP. Should consider AP reboot duration.
-  local total_tries = 75
-  print("\nWiFi connection to AP("..T.SSID..") has failed!")
-
-  --There are many possible disconnect reasons, the following iterates through
-  --the list and returns the string corresponding to the disconnect reason.
-  for key,val in pairs(wifi.eventmon.reason) do
-    if val == T.reason then
-      print("Disconnect reason: "..val.."("..key..")")
-      break
+    if T.reason == wifi.eventmon.reason.ASSOC_LEAVE then
+        --the station has disassociated from a previously connected AP
+        return
     end
-  end
+    -- total_tries: how many times the station will attempt to connect to the AP. Should consider AP reboot duration.
+    local total_tries = 75
+    print("\nWiFi connection to AP("..T.SSID..") has failed!")
 
-  if disconnect_ct == nil then
-    disconnect_ct = 1
-  else
-    disconnect_ct = disconnect_ct + 1
-  end
-  if disconnect_ct < total_tries then
-    print("Retrying connection...(attempt "..(disconnect_ct+1).." of "..total_tries..")")
-  else
-    wifi.sta.disconnect()
-    print("Aborting connection to AP!")
-    disconnect_ct = nil
-  end
+    --There are many possible disconnect reasons, the following iterates through
+    --the list and returns the string corresponding to the disconnect reason.
+    for key,val in pairs(wifi.eventmon.reason) do
+        if val == T.reason then
+            print("Disconnect reason: "..val.."("..key..")")
+            node.restart() 
+            break
+        end
+    end
+
+    if disconnect_ct == nil then
+        disconnect_ct = 1
+    else
+        disconnect_ct = disconnect_ct + 1
+    end
+    if disconnect_ct < total_tries then
+        print("Retrying connection...(attempt "..(disconnect_ct+1).." of "..total_tries..")")
+    else
+        wifi.sta.disconnect()
+        print("Aborting connection to AP!")
+        disconnect_ct = nil
+        node.restart() 
+    end
 end
 -- Register WiFi Station event callbacks
 wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifiConnectEvent)
@@ -92,6 +94,7 @@ dofile("can_a1_test_init4.lua")
 local timer3 = tmr.create()
 timer3:register(600, tmr.ALARM_AUTO, function() if httpMuted==0 then dofile("can_a3_inform_invertor.lua") end end)
 timer3:start()
+dofile("display.lua")
 print("===========================================")
 print("STARTED")
 print("===========================================")
